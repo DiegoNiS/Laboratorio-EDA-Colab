20f58589a99643ca6a4fa6a3e52d227e0863e915
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 12:58:14 -0500

    Variables en java

---
+++ lab01/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+	public static void main (String[] args){
+		int numero = 7;
+		final numero2 = 3;
+		String nombre = "Diego";
+		System.out.println("Número: "+numero);
+		System.out.println("Constante: "+numero2);
+		System.out.println("Nombre: "+nombre);
+	}
+}
