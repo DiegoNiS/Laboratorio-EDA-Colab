431dee5d0954d224d6de8be3b2e7a4725d30e95f
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 12:57:22 -0500

    Salidas en java

---
+++ lab01/Output.java
@@ -0,0 +1,8 @@
+public class Output{
+	public static void main (String[] args){
+		System.out.print("Este es un mensaje sin salto de linea");
+		System.out.println("Este es un mensaje con salto de linea");
+		System.out.println("Número: "+7);
+		System.out.println(4+3);
+	}
+}
