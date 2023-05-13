b6cad48c65553bc2b0d20d732067dfbff935ce63
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 13:39:54 -0500

    Ejemplo usando bucle Foor en java

--- lab01/FoorLoopExample.java
+++ lab01/FoorLoopExample.java
@@ -1,9 +1,8 @@
-public class WhileLoopExample {
+public class ForLoopExample {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
-            i++;
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
     }
 }
