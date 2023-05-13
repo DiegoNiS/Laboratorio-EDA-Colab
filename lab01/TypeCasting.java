a63dc3927221da0fcd2eaf3d72bbea9cbdddb224
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 13:06:31 -0500

    Tipos de Cast en java

---
+++ lab01/TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting{
+	public static void main (String[] args){
+		double dob = 9.8;
+		int doubnum = (int) dob;
+		System.out.println(dob);
+		System.out.println(doubnum);
+	}
+}
