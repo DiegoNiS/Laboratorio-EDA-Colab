82b44312960bf1f0eff922ac12c12f8b8d2d8d52
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 13:03:33 -0500

    Tipos de datos en java

---
+++ lab01/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes{
+	public static void main (String[] args){
+		int numero = 7;
+		double numero2= 3.1416;
+		boolean boleano = false;
+		char let = 'D', let2 = 10;
+		System.out.println("Número: "+numero);
+		System.out.println("Double: "+numero2);
+		System.out.println("Booleano: "+booleano);
+		System.out.println("Chars: "+let+let2);
+	}
+}
