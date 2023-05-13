8ed9dbf865a2aa7ede861adbc058dc5bc7c7af20
Diego Nina <dninas@unsa.edu.pe>
2023-05-13 13:26:46 -0500

    Booleanos en java

---
+++ lab01/Booleans.java
@@ -0,0 +1,13 @@
+public class Booleans {
+    public static void main(String[] args) {
+        boolean esMayor = (10 > 5);
+        System.out.println("¿10 es mayor que 5? " + esMayor);
+        
+        boolean esIgual = (10 == 5);
+        System.out.println("¿10 es igual que 5? " + esIgual);
+        
+        boolean esDiferente = (10 != 5);
+        System.out.println("¿10 es diferente de 5? " + esDiferente);
+    }
+}
+
