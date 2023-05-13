3b0712cbfadabf5dafcb876cd1974ee6bc60f065
Dante <jarednina72@gmail.com>
2023-05-13 14:31:10 -0500

    Metodos en Java

---
+++ lab01/Method.java
@@ -0,0 +1,12 @@
+public class Method{
+    public static void main(String[] args) {
+        int result = addNumbers(5, 3);
+        System.out.println("The result is: " + result);
+    }
+
+    public static int addNumbers(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+}
+
