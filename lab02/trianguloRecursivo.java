559458be98799cc8423fc7cc6cb83341f76b6662
Shavicho <121587829+Shavicho@users.noreply.github.com>
2023-05-17 17:01:45 -0500

    Create trianguloRecursivo.java

---
+++ lab02/trianguloRecursivo.java
@@ -0,0 +1,20 @@
+public class trianguloRecursivo{
+    public static void main(String[] args)
+    {
+        int base=1;
+        trianguloRecursivo(base);
+
+            
+    }
+    public static void trianguloRecursivo(int base)
+    {
+        for(int i=0;i<base;i++)
+            System.out.print("*");
+        System.out.println();
+        if(base<6)
+        {
+            trianguloRecursivo(int base+1);
+
+        }
+    }  
+} 
