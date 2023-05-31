0066f3f8ae9506067ce05398f36e6ba2922bc209
MaxsForocca <121203558+MaxsForocca@users.noreply.github.com>
2023-05-31 17:04:33 -0500

    Iniciando Ejercicio02.java

---
+++ lab03/Ejercicio02/Ejercicio02.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+public class Ejercicio02 {
+    public static void main (String[]args){
+        //probar las pilas
+        Stack<Integer> miPila = new Stack<Integer>();
+        miPila.add(78);
+        
+        System.out.println(miPila.empty());
+    }
+}
