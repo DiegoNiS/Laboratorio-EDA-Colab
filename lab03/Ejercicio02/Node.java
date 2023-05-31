e52a1dd0fd59c26048f02abbaeb56266ae8cedcc
MaxsForocca <121203558+MaxsForocca@users.noreply.github.com>
2023-05-31 17:06:17 -0500

    Iniciando Node.java

---
+++ lab03/Ejercicio02/Node.java
@@ -0,0 +1,21 @@
+// Clase Node (Nodo)
+public class Node <T>{
+    private T dato;
+    private Node<T> next;
+    public Node(T dato, Node<T> next){
+        this.dato = dato;
+        this.next = next;
+    }
+    public T getDato(){
+        return this.dato;
+    }
+    public void setDato(T dato){
+        this.dato = dato;
+    }
+    public Node getNext(){
+        return this.next;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+}
