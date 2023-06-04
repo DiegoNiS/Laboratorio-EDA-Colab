544cade51101efbc7155fb33f3901ca465782df3
Javier Coronado Peña <121587829+Shavicho@users.noreply.github.com>
2023-06-03 22:35:35 -0500

    modificando metodo

--- lab03/Ejercicio01/Ejercicio1.java
+++ lab03/Ejercicio01/Ejercicio1.java
@@ -1,32 +1,40 @@
 
+import java.util.List;
 import java.util.Stack;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
         
         //CREANDO LAS PILAS
-        Stack<int[]> pila1=new Stack<>();
-        Stack<int[]> pila2=new Stack<>();
-        Stack<int[]> pila3=new Stack<>();
+        Stack<Integer> pila1=new Stack<>();
+        Stack<Integer> pila2=new Stack<>();
+        Stack<Integer> pila3=new Stack<>();
         //CREANDO LOS ARRAYS
-        int [] h1={1,1,1,2,3};
-        int [] h2={2,3,4};
-        int [] h3={1,4,1,1};
+        pila1.push(1);
+        pila1.push(1);
+        pila1.push(1);
+        pila1.push(2);
+        pila1.push(3);
         
-        //PUSH PARA INTRODUCIR EL ARRAY EN LA PILA
-        pila1.push(h1);
-        pila2.push(h2);
-        pila3.push(h3);
+        pila2.push(2);
+        pila2.push(3);
+        pila2.push(4);
+        
+        pila3.push(1);
+        pila3.push(4);
+        pila3.push(1);
+        pila3.push(1);
+        
         //IMPRIMIENDO
         
-        System.out.println(equalStack(pila1.pop(),pila2.pop(),pila3.pop()));
+        System.out.println(equalStack(pila1,pila2,pila3));
     }
     
-    static int equalStack(int[]h1,int[]h2,int[]h3){
+    static int equalStack(Stack <Integer> h1,Stack <Integer> h2,Stack <Integer> h3){
         
         //EL CONTADOR TIENE LA FINALIDAD DE BUSCAR EL ESPACIO 
         //EN EL ARRAY PARA CONVERTIRLO A 0
-        int suma1,suma2,suma3,cont1=1,cont2=1,cont3=1;
+        int suma1,suma2,suma3;
         suma1=suma(h1);
         suma2=suma(h2);
         suma3=suma(h3);
@@ -36,34 +44,31 @@ public class Ejercicio1 {
         while(true){
             //CADA IF TIENE LA FUINCION DE COMPARAR CON LOS OTROS ARRAYS
             if(suma1>suma2 || suma1>suma3){
-                h1[h1.length-cont1]=0;
-                suma1=suma(h1);
-                cont1++;
+                h1.pop();
+                suma1=suma(h1); 
             }
             else if(suma2>suma1 || suma2>suma3){
-                h2[h2.length-cont2]=0;
+                h2.pop();
                 suma2=suma(h2);
-                cont2++;
             }
             else if(suma3>suma1 || suma3>suma2){
-                h3[h3.length-cont3]=0;
+                h3.pop();
                 suma3=suma(h3);
-                cont3++;
             }
             //SI LOS TRES ARRAYS SON IGUALES, SE ROMPE EL BUCLE
             else
-                break;    
+                break;  
+
         }
-        System.out.println("La todas las pilas tienen la altura de: ");
+        System.out.println("La altura de todas las pilas es de: ");
         return suma1;
     }
-    //METODO PARA SUMAR EL ARRAY
-    static int suma (int[]h){
+    //METODO PARA SUMAR 
+    static int suma (List <Integer> h){
         
         int suma=0;
-        for (int i = 0; i < h.length; i++)
-            suma+=h[i];
+        for(int altura:h)
+            suma+=altura;
         return suma;
     }
 }
-
