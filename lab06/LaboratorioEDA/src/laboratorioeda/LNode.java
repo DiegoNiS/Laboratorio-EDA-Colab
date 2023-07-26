01801b91ddb9b7c443ebaeab6cc29dacff9e2293
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-26 10:32:43 -0500

    completando LinkedList y LNode

--- lab06/LaboratorioEDA/src/laboratorioeda/LNode.java
+++ lab06/LaboratorioEDA/src/laboratorioeda/LNode.java
@@ -1,5 +1,29 @@
 package laboratorioeda;
 
-public class LNode {
+public class LNode<E> {
+    private E data;
+    private LNode<E> next;
 
+    public LNode(E data, LNode<E> next) {
+    this.data = data;
+    this.next = next;
+    }
+    public LNode(E data) {
+    	this(data, null);
+    }
+    public E getData() {
+    	return this.data;
+    }
+    public void setData(E data) {
+    	this.data = data;
+    }
+    public LNode<E> getNext() {
+	return this.next;
+    }
+    public void setNext(LNode<E> next) {
+	this.next = next;
+    }
+    public String toString() {
+	return this.data.toString();
+    }
 }
--- lab06/LaboratorioEDA/src/laboratorioeda/LinkedList.java
+++ lab06/LaboratorioEDA/src/laboratorioeda/LinkedList.java
@@ -1,21 +1,40 @@
 package laboratorioeda;
 
 public class LinkedList<T> {
+    private LNode<T> head;
     private int items;
     
     public LinkedList(){
         items = 0;
     }
-    
+    public LNode<T> getHead() {
+        return head;
+    }
+
+    public int getItems() {
+        return items;
+    }
     public void add(T num){
-        
+        if (isEmpty()){
+            this.head = new LNode<T>(num, this.head);
+            this.items ++;
+        }
+	    else {
+            LNode<T> aux = this.head;
+            while ( aux.getNext() != null)
+		        aux = aux.getNext();
+            aux.setNext(new LNode<T>(num));
+            this.items ++;
+        }    
     }
     public boolean isEmpty(){
         return this.items == 0;
     }
     
     public String toString(){
-        
-        return " ";
+        String str = "";
+        for(LNode<T> aux = this.head; aux != null; aux = aux.getNext())
+            str += aux.toString() + ", ";
+        return str;
     }
 }
